7bfa2f57580b42befff0579302774252f60597eb
cartige <cartiaux.gerald@gmail.com>
2022-07-27 18:29:23 +0200

    cssScript

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-
-let grid = new Game(10,10,3,4);
+let grid = new Game(10,10,3,3);
 console.log(grid.grid);
-console.log(grid.rules1);
+console.log(grid.minCellToBorn);
+
+
+function drawGrid(gridID){
+    let table = document.getElementById(gridID);
+    table.innerHTML = '';
+    let gridHeight = grid.cells.length;
+    for(let i = 0 ; i < gridHeight ; i++){
+        let newLine = table.insertRow(i);
+        let gridWidth = grid.cells[i].length;
+        for (let j = 0 ; j < gridWidth ; j++){
+            let newCell = newLine.insertCell(j);
+            if(grid.cells[i][j]){
+                newCell.classList.add('alive');
+            }
+        }
+    }
+}
+
+console.log(grid.nbCellsAlive(1, 2));
--- script.js
+++ script.js
@@ -1,29 +1,79 @@
 class Game{
-    constructor(width, heigth, min_cell_to_born, min_cell_to_born){
+    constructor(width, heigth, minCellToBorn, minCellToStay){
         this.width = width;
         this.heigth = heigth;
-        this.cells = [...Array(this.width)].map(cell => Array(this.heigth));
-        this._min_cell_to_born = min_cell_to_born;
-        this._min_cell_to_stay = min_cell_to_stay;
+        // this.cells = [...Array(this.width)].map(cell => Array(this.heigth));
+        this.cells = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 1, 0, 1, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+                      [1, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+                      [0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+                      [1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+                      [0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]];
+        this._minCellToBorn = minCellToBorn;
+        this._minCellToStay = minCellToStay;
     }
     get grid() {
         return this.cells;
     }
-    get min_cell_to_born() {
-        return this._min_cell_to_born;
+    get minCellToBorn() {
+        return this._minCellToBorn;
     }
-    get min_cell_to_stay() {
-        return this._min_cell_to_stay;
+    get minCellToStay() {
+        return this._minCellToStay;
     }
-    set min_cell_to_born(min_cell_to_born) {
-        if(min_cell_to_born !== this._min_cell_to_born){
-            this._min_cell_to_born = min_cell_to_born;
+    set minCellToBorn(minCellToBorn) {
+        if(minCellToBorn !== this._minCellToBorn){
+            this._minCellToBorn = minCellToBorn;
         }
     }
-    set min_cell_to_stay(min_cell_to_stay) {
-        if(min_cell_to_stay !== this._min_cell_to_stay){
-            this._min_cell_to_stay = min_cell_to_stay;
+    set minCellToStay(minCellToStay) {
+        if(minCellToStay !== this._minCellToStay){
+            this._minCellToStay = minCellToStay;
         }
     }
 
+    nbCellsAlive(i, j){
+        let result = 0;
+        for(let y = Math.max(i-1, 0) ; y <= Math.min(i+1, this.cells.length-1) ; y++){
+
+            for(let x = Math.max(j-1, 0) ; x <= Math.min(j+1, this.cells[y].length-1) ; x++){
+  
+                result += this.cells[y][x];
+            }
+        }     
+        
+        return result-this.cells[i][j];
+    }
+
+    computeNextGeneration(){
+
+        let newGeneration = [];
+        this.cells.forEach((row, i) => {
+            let newRow = [];
+            newGeneration.push(newRow);
+            row.forEach((cell, j) => {
+    
+                if(cell){
+                    if(this._minCellToStay === this.nbCellsAlive(i, j) || this._minCellToStay-1 === this.nbCellsAlive(i, j)){
+                        newRow.push(1);
+                    }else{
+                        newRow.push(0);
+                    }
+                }else{
+                    if(this._minCellToBorn === this.nbCellsAlive(i, j)){
+                        newRow.push(1);
+                    }else{
+                        newRow.push(0);
+                    }
+                }
+            })
+        })
+        this.cells = newGeneration;
+    }
+
 }
+
